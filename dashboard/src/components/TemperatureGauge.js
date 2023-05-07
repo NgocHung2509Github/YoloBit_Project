773d7fb64cd2eafc5d391c043fb94fecc8d19a72
ngochung <hungnguyen.nnh2509@gmail.com>
2023-05-07 15:40:02 +0700

    dashboard UI

---
+++ dashboard/src/components/TemperatureGauge.js
@@ -0,0 +1,45 @@
+import { CircularProgress, Typography } from '@mui/material';
+
+function CircularProgressWithLabel(props) {
+  const { value, temperature } = props;
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        display: 'inline-block',
+        backgroundColor: '#f5f5f5',
+        borderRadius: '50%',
+        padding: '10px',
+      }}
+    >
+      <CircularProgress variant="determinate" value={value} {...props} sx={{ color: '#0E8388', thickness: 4 }} />
+      <Typography
+        variant="h5"
+        component="div"
+        style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}
+      >
+        {temperature}°C
+      </Typography>
+    </div>
+  );
+}
+
+function TemperatureGauge({ temperature }) {
+  const maxTemperature = 60;
+  const threshold = 70;
+
+  // Convert Celsius to a 0 to 60 scale
+  const value = (temperature / maxTemperature) * 100;
+
+  // Determine color based on threshold
+  const color = value >= threshold ? 'error' : 'primary';
+
+  return (
+    <CircularProgressWithLabel value={value} temperature={temperature} color={color} size={100} />
+  );
+}
+
+
+
+export default TemperatureGauge;
